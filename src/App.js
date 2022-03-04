6ae0257eb89e89ae6bd0108b1db2b659e5d047a6
Justin Sun <justinsunyt@gmail.com>
2022-03-03 23:37:48 -0800

    FIRST TIME LETS FUCKING GO
    Co-authored-by: helena zhou <zhouhelena@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  let heyBoyfriend = new Date("July 29, 2021 08:28:00");
+  let heyBoyfriendUTC = Date.UTC(
+    heyBoyfriend.getFullYear(),
+    heyBoyfriend.getMonth(),
+    heyBoyfriend.getDate()
+  );
+
+  const [day, setDay] = useState(findDateDiff());
+
+  function findDateDiff() {
+    let now = new Date();
+    let nowUTC = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate());
+    let dayInt = 1000 * 60 * 60 * 24;
+    return (nowUTC - heyBoyfriendUTC) / dayInt;
+  }
+
+  useEffect(() => {
+    setInterval(() => setDay(findDateDiff()), 1000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h1>{day} days since "hey boyfriend" and "hey girlf" </h1>
+      </center>
     </div>
   );
 }
